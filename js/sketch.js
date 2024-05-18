ecb72982eddde6d4168e7c917496cfe61b642372
dev-joseh <hiquess@hotmail.com>
2024-05-18 12:27:46 -0300

    Finished the drawBlock function and the color picker.

--- js/sketch.js
+++ js/sketch.js
@@ -1,15 +1,27 @@
 
 const grid = document.querySelector("#grid .content .sketch");
+const displayNumberOfBlocks = document.querySelectorAll("#grid .content .input span");
 
 function changeGridLayout(num) {
+    grid.innerHTML="";
+
+    displayNumberOfBlocks[0].innerText = num.toString();
+    displayNumberOfBlocks[1].innerText = num.toString();
     for(let i=0;i<num;i++) {
-            const child = grid.appendChild(document.createElement("div"));
+            const row = grid.appendChild(document.createElement("div"));
             for(let i=0;i<num;i++) {
-                child.appendChild(document.createElement("div"));
+                const block = row.appendChild(document.createElement("div"));
+                block.addEventListener("mouseenter", (e) => {
+                    if(e.buttons==1)
+                        drawBlock(block);
+                });
+                block.addEventListener("click", () => {drawBlock(block)});
             }
         }
 }
 
-let numberOfBlocks = 6;
+const inputColor = document.querySelector("#html5colorpicker");
 
-changeGridLayout(numberOfBlocks);
+function drawBlock(block) {
+    block.style.backgroundColor = inputColor.value;
+}
