f8103b625ea4fdce0a8106f6c1a4aa32b8a5f086
dev-joseh <hiquess@hotmail.com>
2024-05-18 11:33:55 -0300

    Added the sketch and a slider to input number of + blocks in the sketch.

--- js/sketch.js
+++ js/sketch.js
@@ -1,1 +1,15 @@
 
+const grid = document.querySelector("#grid .content .sketch");
+
+function changeGridLayout(num) {
+    for(let i=0;i<num;i++) {
+            const child = grid.appendChild(document.createElement("div"));
+            for(let i=0;i<num;i++) {
+                child.appendChild(document.createElement("div"));
+            }
+        }
+}
+
+let numberOfBlocks = 6;
+
+changeGridLayout(numberOfBlocks);
