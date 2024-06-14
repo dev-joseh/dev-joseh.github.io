0353e90ec6df25f468a96352940170822e4bbed8
dev-joseh <hiquess@hotmail.com>
2024-06-14 15:56:20 -0300

    Changed the default theme to light mode

--- js/theme-button.js
+++ js/theme-button.js
@@ -2,5 +2,5 @@ const checkbox = document.querySelector("#checkTheme");
 const root = document.querySelector(":root");
 
 checkbox.addEventListener("change", ()=> {
-    root.classList.toggle("light");
+    root.classList.toggle("dark");
 })
